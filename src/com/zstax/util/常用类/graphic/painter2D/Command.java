ef631b8022a79c542979744b6a44df0ca5792d86
zhuliangxuan <zhuliangxuan@zstax.com>
2017-06-07 16:32:50 +0800

    整合

--- src/com/zstax/util/常用类/graphic/painter2D/Command.java
+++
@@ -1,9 +0,0 @@
-package com.zstax.util.常用类.graphic.painter2D;
-/**
- * �������LINE��ʾ���ߣ�CIRCLE��ʾ��Բ��RECTANGLE��ʾ������
- */
-public interface Command {
-	public static final int LINE = 2; 
-	public static final int CIRCLE = 4; 
-	public static final int RECTANGLE = 8; 
-}
